f1d9a6059cbc363a6234f4a22b3c9fd6845eaa70
OvieMudi <oviisaacs@gmail.com>
2019-12-03 09:01:35 +0100

    venues: implement basic crud for venues

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
 from flask_moment import Moment
 from models import db, Artist, Venue, Show
 from flask_migrate import Migrate
@@ -218,7 +218,7 @@ def create_venue_submission():
     flash('Venue ' + request.form['name'] + ' was successfully listed!')
 
   except:
-    flash('An error occurred. Venue ' + new_venue.name + ' could not be listed.')
+    flash('An error occurred. Venue ' + new_venue.name + ' could not be listed.', category='error')
     print('exc_info():==========', exc_info())
     db.session.rollback()
 
@@ -232,12 +232,26 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  status = False
+  try:
+    venue = Venue.query.get(venue_id)
+    db.session.delete(venue)
+    db.session.commit()
+    status = True
+    flash('Venue successfully deleted!')
 
-  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-  # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  except:
+    print(exc_info())
+    db.session.rollback()
+    status = False
+    flash('Error deleting venue', category='error')
+
+  finally:
+    db.session.close()
+  
+  return jsonify({
+    'success': status
+    })
 
 #  Artists
 #  ----------------------------------------------------------------
@@ -355,20 +369,7 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
+  venue = Venue.query.get(venue_id)
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -398,7 +399,7 @@ def edit_venue_submission(venue_id):
     flash('Venue ' + request.form['name'] + ' was successfully updated!')
 
   except:
-    flash('An error occurred. Venue ' + request.form['name'] + ' could not be updated.')
+    flash('An error occurred. Venue ' + request.form['name'] + ' could not be updated.', category='error')
     print('exc_info():==========', exc_info())
     db.session.rollback()
 
